d3aaaa9f8c1c3c800cb56d98ffef733d4a37d80e
SipuliCodes <vepsalainensisu@gmail.com>
2024-08-26 20:39:27 +0300

    main page done

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Routes, Route, useNavigate} from 'react-router-dom'
+import React from 'react'
+
+import './App.css'
+
+import MainPage from './Components/MainPage/MainPage'
+
+const App = () => {
+  const navigate = useNavigate();
+
+  const linkClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    navigate(`/${event.currentTarget.id}`)
+  }
+
+  return (
+    <div className='app-grid-container'>
+      <div className='navigation-bar'>
+        <button className='navigation-bar-item' id='main' onClick={linkClick}>Main</button>
+      </div>
+      <Routes>
+        <Route path='/main' element={<MainPage />} />
+        <Route path='*' element={<MainPage />} />
+      </Routes>
+      <div className='footer-flex'>
+        <div className='footer'>
+          <h3>Sisu Vepsäläinen © 2024</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/MainPage/MainPage.tsx
@@ -0,0 +1,46 @@
+import './MainPage.css'
+
+import CsharpIcon from '../../assets/Csharp.svg'
+import JavaIcon from '../../assets/java.svg'
+import AzureIcon from '../../assets/Microsoft_Azure.svg'
+import MongoDbIcon from '../../assets/mongodb.svg'
+import NodeJsIcon from '../../assets/nodejs.svg'
+import PythonIcon from '../../assets/python.svg'
+import ReactIcon from '../../assets/React-icon.svg'
+import TSIcon from '../../assets/typescript.svg'
+import Me from '../../assets/me.jpg'
+
+const listOfExperience: string[] = [
+  ReactIcon,
+  TSIcon,
+  MongoDbIcon,
+  NodeJsIcon,
+  PythonIcon,
+  CsharpIcon,
+  JavaIcon,
+  AzureIcon
+]
+
+const MainPage = () => {
+  return (
+    <div className="main-page-grid">
+      <div className='main-page-experience-flex'>
+        <h2>Experience</h2>
+        <div className='main-page-experience-icon-grid'>
+          {listOfExperience.map(experience => <img className='main-page-experience-icon' src={experience}></img>)}
+        </div>
+      </div>
+      <div className='main-page-details-flex'>
+        <h2>Sisu Vepsälinen</h2>
+        <img className='main-page-details-image' src={Me}></img>
+        <h3>ICT student at University of Turku</h3>
+        <div className="contact-bar">
+          <a className='contact-bar-link' href="https://www.linkedin.com/in/sisu-veps%C3%A4l%C3%A4inen-654b02238/">LinkedIn</a>
+          <a className='contact-bar-link' href="https://github.com/SipuliCodes">GitHub</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
